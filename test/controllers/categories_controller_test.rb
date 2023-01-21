f8f5bb78985543c798cfa7720202167f81f64d80
huyhoan1109 <huyhoan0901@gmail.com>
2023-01-21 15:19:56 +0700

    update models

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class ListControllerTest < ActionDispatch::IntegrationTest
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
